cc7a4ee868ad86fb02c95e356ad77acb392c4100
safwan <safwan0491@gmail.com>
2025-01-06 20:52:21 -0900

    Updated Todo App: Improved Duplicate Handling and Toast Styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = todo => {
+  const addTodo = (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
 
-    const newTodos = [todo, ...todos];
+    // Check for duplicates
+    const isDuplicate = todos.some((item) => item.text.toLowerCase() === todo.text.toLowerCase());
 
+    if (isDuplicate) {
+      toast.error('Duplicate Todo! ', {
+        position: 'top-center',
+        autoClose: 1000,
+
+        hideProgressBar: true,
+        style: {
+          backgroundColor: '#f8d7da',
+          color: '#721c24',
+          borderRadius: '8px',
+        },
+      });
+      return;
+    }
+   
+
+
+
+    const newTodos = [todo, ...todos];
     setTodos(newTodos);
-    console.log(...todos);
   };
 
   const updateTodo = (todoId, newValue) => {
@@ -21,17 +42,18 @@ function TodoList() {
       return;
     }
 
-    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
   };
 
-  const removeTodo = id => {
-    const removedArr = [...todos].filter(todo => todo.id !== id);
-
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
     setTodos(removedArr);
   };
 
-  const completeTodo = id => {
-    let updatedTodos = todos.map(todo => {
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete;
       }
@@ -42,10 +64,9 @@ function TodoList() {
 
   return (
     <div>
-      <div className='header-box'>
+      <div className="header-box">
         <h1>What's the Plan For Today?</h1>
-        </div>
-
+      </div>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
@@ -53,8 +74,11 @@ function TodoList() {
         removeTodo={removeTodo}
         updateTodo={updateTodo}
       />
+      {/* Toast Container */}
+      <ToastContainer />
     </div>
   );
 }
 
 export default TodoList;
+
